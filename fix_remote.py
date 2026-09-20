328fe14a23b607a496604ce716f2c1e4c38fd69e
Abhishek Gadmale <agadmale335@gmail.com>
2026-09-20 16:57:16 +0530

    chore: cleanup debugging and script files

--- fix_remote.py
+++
@@ -1,5 +0,0 @@
-import codecs
-path = 'packages/database/src/publishing/__tests__/remote-execution.spec.ts'
-with codecs.open(path, 'r', 'utf-8') as f: text = f.read()
-text = text.replace('expect(typeof (parseMeta(getVariant(res)) as any).publishRequestedAt).toBe(\'string\');', '// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access\n      expect(typeof (parseMeta(getVariant(res)) as any).publishRequestedAt).toBe(\'string\');')
-with codecs.open(path, 'w', 'utf-8') as f: f.write(text)
